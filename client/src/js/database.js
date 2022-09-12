5cea18d01f187493e5d8bf9bade95b99408c5ab7
Jordan D <degco44@gmail.com>
2022-09-11 22:44:08 -0500

    functions for GET and PUT

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,29 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('Data saved to database', result.value);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Get all content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('Data retrieved from database', result.value)
+    : console.log('Data not found in database');
+  return result.value;
+};
 
 initdb();
